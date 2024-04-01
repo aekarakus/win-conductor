550926de746175f9aeb08564173ba1ec589c2399
aeren <alperenkarakus@aol.com>
2024-04-01 23:46:06 +0300

    fix: repositories

---
+++ src/main/java/io/aekarakus/domain/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package io.aekarakus.domain.repositories;
+
+import io.aekarakus.domain.models.Application;
+import io.aekarakus.domain.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Long, User> {
+}
