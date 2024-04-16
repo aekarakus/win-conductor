8af84afe578dd74b4e97e0e5143f0d4296a50e44
aeren <alperenkarakus@aol.com>
2024-04-16 22:41:31 +0300

    chore: small edits

--- src/main/java/io/aekarakus/domain/mappers/DeviceMapper.java
+++ src/main/java/io/aekarakus/domain/mappers/DeviceMapper.java
@@ -1,17 +1,10 @@
 package io.aekarakus.domain.mappers;
 
-import io.aekarakus.domain.DeviceInfo;
 import io.aekarakus.domain.ProfileInfo;
 import io.aekarakus.domain.dtos.DeviceDto;
-import io.aekarakus.domain.dtos.ProfileDto;
 import io.aekarakus.domain.models.Device;
 import io.aekarakus.domain.models.Profile;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.mapstruct.*;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
