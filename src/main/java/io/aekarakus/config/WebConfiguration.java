57ca22d725aad3aae07be6d8d9d575eb7ebbc80c
aeren <alperenkarakus@aol.com>
2024-04-24 23:02:11 +0300

    fix: rest call

---
+++ src/main/java/io/aekarakus/config/WebConfiguration.java
@@ -0,0 +1,11 @@
+package io.aekarakus.config;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
+
