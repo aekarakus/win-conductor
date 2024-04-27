2d47721512a6a68b0a3e4180231186be159f08c5
aeren <alperenkarakus@aol.com>
2024-04-27 22:45:23 +0300

    style: minor theming

--- gui/src/layouts/RootLayout.jsx
+++ gui/src/layouts/RootLayout.jsx
@@ -2,13 +2,13 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import { Flex } from "@chakra-ui/react";
 
-export default function RootLayout(){
+export default function RootLayout() {
     return (
         <>
-        <Flex direction="row" w="full" h="full">
-        <Sidebar />
-        <Outlet />
-        </Flex>
+            <Flex direction="row" w="full" h="full">
+                <Sidebar />
+                <Outlet />
+            </Flex>
         </>
     );
 }
--- gui/src/main.jsx
+++ gui/src/main.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      html: {
+        width: '100%',
+        height: '100%',
+      },
+      body: {
+        bg: '#f3f3f3',
+        width: '100%',
+        height: '100%',
+      },
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
