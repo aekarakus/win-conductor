989753fc90433e24f2a99c87c37dc1ac9d9e1090
aeren <alperenkarakus@aol.com>
2024-05-05 16:17:08 +0300

    chore: added sentinel module

--- sentinel/src/main/java/io/aekarakus/SentinelApplication.java
+++ sentinel/src/main/java/io/aekarakus/SentinelApplication.java
@@ -1,7 +1,7 @@
 package io.aekarakus;
 
-public class Main {
+public class SentinelApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringAppli
     }
 }
