ac3915fb18fd753d181d50185e774e4799a1fc5d
aeren <alperenkarakus@aol.com>
2024-05-06 00:31:45 +0300

    chore: readded files

---
+++ sentinel/src/main/java/io/aekarakus/winconductor/sentinel/services/PowerShellServiceImpl.java
@@ -0,0 +1,35 @@
+package io.aekarakus.winconductor.sentinel.services;
+
+import io.aekarakus.winconductor.sentinel.entities.Command;
+import io.aekarakus.winconductor.sentinel.entities.Device;
+import io.aekarakus.winconductor.sentinel.entities.dtos.CommandContext;
+import io.cloudsoft.winrm4j.client.WinRmClientContext;
+import io.cloudsoft.winrm4j.winrm.WinRmTool;
+import io.cloudsoft.winrm4j.winrm.WinRmToolResponse;
+import org.apache.http.client.config.AuthSchemes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PowerShellServiceImpl implements PowerShellService {
+    @Override
+    public void sendCommandToMachine(CommandContext commandContext) {
+
+        WinRmClientContext context = WinRmClientContext.newInstance();
+        Device device = commandContext.getDevice();
+        Command command = commandContext.getCommand();
+
+        WinRmTool tool = WinRmTool.Builder.builder(device.getAddress(), device.getUserName(), device.getPassword())
+                .authenticationScheme(AuthSchemes.NTLM)
+                .port(5985)
+                .context(context)
+                .build();
+
+        WinRmToolResponse response = tool.executePs(command.getContent());
+
+        System.out.println("Out:" + response.getStdOut());
+        System.out.println("status:" + response.getStatusCode());
+        System.out.println("Err:" + response.getStdErr());
+
+        context.shutdown();
+    }
+}
